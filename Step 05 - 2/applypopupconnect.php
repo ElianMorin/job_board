20ab1afbdc7110a3d8b9ad92dab119c1c923995f
Elian Morin <elian.morin@epitech.eu>
2018-09-27 18:25:51 +0200

    Apply popup mise en page, formulaire inscription+contact et formulaire juste contact (quand déja inscrit)

--- Step 05 - 2/applypopupconnect.php
+++ Step 05 - 2/applypopupconnect.php
@@ -12,7 +12,7 @@
              <link rel="stylesheet" href="css/content.css">
              <link rel="stylesheet" href="css/navbar.css">
              <link rel="stylesheet" href="css/header.css">
-             <link rel="stylesheet" href="css/applypopup.css">
+             <link rel="stylesheet" href="css/applypopupconnect.css">
              <title>LeBonJob</title>
              <script>
 
@@ -25,10 +25,19 @@
     </head>
     <body>
       <div id="modal1" class="modal">
-        <h1 class="center-align">Contact</h1>
+        <h3 class="center-align">Contact/Inscription</h3>
           <form action="/action_page.php">
           <label for="fname">Nom & prénom</label>
           <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom">
+
+          <label for="sujet">Mot de passe</label>
+          <input type="password" id="password" name="password" placeholder="Votre mot de passe">
+
+          <label for="sujet">Numéro de téléphone</label>
+          <input type="tel" id="numero" name="numero" placeholder="Votre numéro de téléphone">
+
+          <label for="emailAddress">Email</label>
+          <input id="emailAddress" type="email" name="email" placeholder="Votre email">
 
           <label for="subject">Message</label>
           <textarea id="message" name="message" placeholder="Votre message" style="height:200px"></textarea>
